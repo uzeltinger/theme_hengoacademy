7e0891470bdb6c9763be3819e2a61869d6cf90db
Uzeltinger <fabiouz@gmail.com>
2022-07-28 16:12:52 -0300

    inicio

---
+++ lib.php
@@ -0,0 +1,102 @@
+<?php
+
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+
+// We will add callbacks here as we add features to our theme.
+
+/**
+ * Returns the main SCSS content.
+ *
+ * @param theme_config $theme The theme config object.
+ * @return string
+ */
+function theme_hengoacademy_get_main_scss_content($theme)
+{
+    global $CFG;
+
+    $scss = '';
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+    $fs = get_file_storage();
+
+    $context = context_system::instance();
+    if ($filename == 'default.scss') {
+        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    } else if ($filename == 'plain.scss') {
+        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_hengoacademy', 'preset', 0, '/', $filename))) {
+        // This preset file was fetched from the file area for theme_hengoacademy and not theme_boost (see the line above).
+        $scss .= $presetfile->get_content();
+    } else {
+        // Safety fallback - maybe new installs etc.
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    }
+
+    // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.
+    $pre = file_get_contents($CFG->dirroot . '/theme/hengoacademy/scss/pre.scss');
+    // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.
+    $post = file_get_contents($CFG->dirroot . '/theme/hengoacademy/scss/post.scss');
+
+    // Combine them together.
+    return $pre . "\n" . $scss . "\n" . $post;
+}
+
+
+/**
+ * Copy the updated theme image to the correct location in dataroot for the image to be served
+ * by /theme/image.php. Also clear theme caches.
+ *
+ * @param $settingname
+ */
+function theme_hengoacademy_update_settings_images($settingname) {
+    global $CFG;
+
+    // The setting name that was updated comes as a string like 's_theme_hengoacademy_loginbackgroundimage'.
+    // We split it on '_' characters.
+    $parts = explode('_', $settingname);
+    // And get the last one to get the setting name..
+    $settingname = end($parts);
+
+    // Admin settings are stored in system context.
+    $syscontext = context_system::instance();
+    // This is the component name the setting is stored in.
+    $component = 'theme_hengoacademy';
+
+
+    // This is the value of the admin setting which is the filename of the uploaded file.
+    $filename = get_config($component, $settingname);
+    // We extract the file extension because we want to preserve it.
+    $extension = substr($filename, strrpos($filename, '.') + 1);
+
+    // This is the path in the moodle internal file system.
+    $fullpath = "/{$syscontext->id}/{$component}/{$settingname}/0{$filename}";
+
+    // This location matches the searched for location in theme_config::resolve_image_location.
+    $pathname = $CFG->dataroot . '/pix_plugins/theme/hengoacademy/' . $settingname . '.' . $extension;
+
+    // This pattern matches any previous files with maybe different file extensions.
+    $pathpattern = $CFG->dataroot . '/pix_plugins/theme/hengoacademy/' . $settingname . '.*';
+
+    // Make sure this dir exists.
+    @mkdir($CFG->dataroot . '/pix_plugins/theme/hengoacademy/', $CFG->directorypermissions, true);
+
+    // Delete any existing files for this setting.
+    foreach (glob($pathpattern) as $filename) {
+        @unlink($filename);
+    }
+
+    // Get an instance of the moodle file storage.
+    $fs = get_file_storage();
+    // This is an efficient way to get a file if we know the exact path.
+    if ($file = $fs->get_file_by_hash(sha1($fullpath))) {
+        // We got the stored file - copy it to dataroot.
+        $file->copy_content_to($pathname);
+    }
+
+    // Reset theme caches.
+    theme_reset_all_caches();
+}
